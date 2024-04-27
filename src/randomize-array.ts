8fe2ef53eaf8a251cef3b15434fc8d1f46932765
Thomas Riffard <ferdodo@hotmail.fr>
2024-04-27 19:55:06 +0200

    fix documentation

--- src/randomize-array.ts
+++ src/randomize-array.ts
@@ -3,7 +3,7 @@ import { randomInteger } from "daily-prng";
 /**
  * Returns a new randomized array.
  *
- * @includeExample ./src/random-pick.example.ts
+ * @includeExample ./src/randomize-array.example.ts
  */
 export function randomizeArray<T>(arr: T[]): T[] {
 	return [...arr].sort(() => randomInteger(0, 2) - 0.5);
