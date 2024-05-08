b958172630b0e2c562b37769413949b067313f7d
Thomas Riffard <ferdodo@hotmail.fr>
2024-05-08 22:27:13 +0200

    📝 include example

--- src/random-float.ts
+++ src/random-float.ts
@@ -6,6 +6,8 @@ const random = new Randoma({ seed });
 
 /**
  * Returns a random float between min (inclusive) and max (exclusive).
+ *
+ * @includeExample ./src/random-float.example.ts
  */
 export function randomFloat(min: number, max: number): number {
 	return random.floatInRange(min, max);
--- src/random-integer.ts
+++ src/random-integer.ts
@@ -8,9 +8,6 @@ const random = new Randoma({ seed });
  * Returns a random integer between min (inclusive) and max (exclusive).
  *
  * @includeExample ./src/random-integer.example.ts
- * @param min - The minimum value.
- * @param max - The maximum value.
- * @returns A random integer.
  */
 export function randomInteger(min: number, max: number): number {
 	return random.integerInRange(min, max - 1);
